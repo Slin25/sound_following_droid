cf1b89104151aef5348d3109a8f1574303a712d6
Shang <slin2515@gmail.com>
2019-11-17 22:20:20 -0600

    created function for ambient

---
+++ analogRead/analogRead.ino
@@ -0,0 +1,34 @@
+#define mic1 A1
+#define mic2 A2
+
+define int ambientLevel
+
+// setting initial ambient value 
+int collectAmbient() {
+  int initAmbient = 0;
+  // delay for 1 sec (1000 milli sec)
+  delay(1000);
+
+  // collect 1000 ambient values thent take average
+  for (int i = 0; i < 1000; i++) {
+    initAmbient += analogRead(mic1);
+  }
+  return (initAmbient/1000);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // rate to read the data coming in
+  Serial.begin(9600);
+
+  ambientLevel = collectAmbient();
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // show the graph of the mics
+  Serial.println(analogRead(mic1));
+}
