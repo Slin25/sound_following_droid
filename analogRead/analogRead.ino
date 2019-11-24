7ad7ac356760f35aca77b94b650bca7f1c9d3b98
Shang <slin2515@gmail.com>
2019-11-24 02:25:02 -0600

    initial attempt at isClap function & added moveForward

--- analogRead/analogRead.ino
+++ analogRead/analogRead.ino
@@ -1,5 +1,7 @@
 #define mic1 A1
 #define mic2 A2
+// mic1 - left
+// mic2 - right
 
 int ambientLevel;
 
@@ -9,25 +11,36 @@ int collectAmbient() {
   // delay for 1 sec (1000 milli sec)
   delay(1000);
 
-  // collect 1000 ambient values thent take average
+  // collect 1000 ambient values then take average
   for (int i = 0; i < 1000; i++) {
     initAmbient += analogRead(mic1);
   }
   return (initAmbient/1000);
 }
-
+void moveForward() {
+  
+}
 void turnLeft() {
   // pin 5-connected to left wheel, right wheel is faster 
   digitalWrite(5, LOW);
+  moveForward();
 }
 
 void turnRight() {
   // pin 6-connected to right wheel, left wheel is faster 
   digitalWrite(6, LOW);
+  moveForward();
 }
 
-int isClap(double mic1, double mic2) {
-
+void isClap(double mic1, double mic2) {
+  // above the threshold then it's considered a clap
+  if (mic1 > (ambientLevel * 3) || mic2 > (ambientLevel*3)) {
+    if (mic1 > mic2) {
+      turnLeft();
+    } else if (mic2 > mic1) {
+      turnRight();
+    }
+  }
 }
 
 void setup() {
@@ -35,7 +48,7 @@ void setup() {
   
   // rate to read the data coming in
   Serial.begin(9600);
-
+  // read the stable voltage level
   ambientLevel = collectAmbient();
   
 }
@@ -44,7 +57,8 @@ void loop() {
   // put your main code here, to run repeatedly:
   double mic1Data = analogRead(mic1) - ambientLevel;
   double mic2Data = analogRead(mic2) - ambientLevel;
-
+  
+  // making all the voltages positive
   if (mic1Data < 0) {
     mic1Data = -1 * mic1Data;
   }
